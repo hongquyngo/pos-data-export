3d8826cc6d54750a8b03b6aa7b02f2806e8efc34
hongquyngo <quy.ngo@prostech-asia.com>
2025-04-15 00:22:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,26 @@
-# config.py
 import os
+import json
 from dotenv import load_dotenv
+import streamlit as st
 
-# Load .env file
+# Load từ file .env khi chạy local
 load_dotenv()
 
-DB_CONFIG = {
-    "host": "erp-all-production.cx1uaj6vj8s5.ap-southeast-1.rds.amazonaws.com",
-    "port": 3306,
-    "user": "streamlit_user",
-    "password": os.getenv("DB_PASSWORD"),
-    "database": "prostechvn"
-}
+# Đọc từ secrets nếu deploy Streamlit Cloud
+def is_running_on_cloud():
+    return "DB_CONFIG" in st.secrets
 
-EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+if is_running_on_cloud():
+    DB_CONFIG = st.secrets["DB_CONFIG"]
+    EXCHANGE_RATE_API_KEY = st.secrets["API"]["EXCHANGE_RATE_API_KEY"]
+    GOOGLE_SERVICE_ACCOUNT_JSON = st.secrets["GOOGLE_SERVICE_ACCOUNT_JSON"]
+else:
+    DB_CONFIG = {
+        "host": "erp-all-production.cx1uaj6vj8s5.ap-southeast-1.rds.amazonaws.com",
+        "port": 3306,
+        "user": "streamlit_user",
+        "password": os.getenv("DB_PASSWORD"),
+        "database": "prostechvn"
+    }
+    EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+    GOOGLE_SERVICE_ACCOUNT_JSON = open("credentials.json").read()
